f17e17daae62c501d8eed9253437bd4d0d323063
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-09-07 23:05:41 +0100

    add sums

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,7 @@
-function add(num1, num2) {
+function add(a, b) {
     // let result = num1 + num2;
-    return num1 + num2;
+    return a + b;
 }
 
-let result = sum(a, b);
+let result = add(a, b);
 console.log(result);
