1ece44beafe26e5e8be5670885f615a6c0e6f20f
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-09-07 15:50:23 +0100

    loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -12,22 +12,23 @@ function multi_languages () {
 }
 */
 
+/*
 for (lang === 1; lang <= 3; lang++) {
     console.log ('C is fun');
     console.log ('Python is cool');
     console.log ('JavaScript is amazing');
 }
+*/
 
-/*
+
 for (first_line == 1; first_line <= 3; first_line++) {
     console.log('C is fun');
 }
 
 for (second_line == 1; second_line <= 3; second_line++) {
-    console.log("Python is cool");
+    console.log('Python is cool');
 }
 
 for (third_line == 1; third_line <= 3; third_line++) {
-    console.log("JavasScript is amazing");
+    console.log('JavasScript is amazing');
 }
-*/
