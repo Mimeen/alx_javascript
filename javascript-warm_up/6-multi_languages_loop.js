efe6166b26bd50e9054cb606470ce1a86f087c56
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-09-11 15:19:17 +0100

    loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,5 @@
-let first_line = 'C is fun';
-let second_line = 'Python is cool';
-let third_line = 'JavaScript is amazing';
+const lang = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-multi_languages = [first_line, second_line, third_line];
-
-let n;
-
-for (n === 1; n <= multi_languages.length; n++) {
-    console.log (multi_languages);
+for (let x = 0; x < lang.length; x++) {
+    console.log (length[x]);
 }
