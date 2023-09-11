27d996264fa4d02af97c7870c407b17356031cbf
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-09-11 15:04:49 +0100

    loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,39 +1,11 @@
+multi_languages = [first_line, second_line, third_line];
+
 let first_line = 'C is fun';
 let second_line = 'Python is cool';
 let third_line = 'JavaScript is amazing';
 
 let n;
 
-for (n === 1; n <= 3; n++) {
-    // console.log ('${first_line}\n${second_line}$\n{third_line}');
-    console.log (first_line + '\n' + second_line + '\n' + third_line);
+for (n === 1; n <= multi_languages.length; n++) {
+    console.log (multi_languages);
 }
-/*
-function multi_languages () {
-    console.log("C is fun");
-    console.log("Python is cool");
-    console.log("JavaScript is amazing");
-}
-*/
-
-/*
-for (lang === 1; lang <= 3; lang++) {
-    console.log ('C is fun');
-    console.log ('Python is cool');
-    console.log ('JavaScript is amazing');
-}
-*/
-
-/*
-for (first_line == 1; first_line <= 3; first_line++) {
-    console.log('C is fun');
-}
-
-for (second_line == 1; second_line <= 3; second_line++) {
-    console.log('Python is cool');
-}
-
-for (third_line == 1; third_line <= 3; third_line++) {
-    console.log('JavasScript is amazing');
-}
-*/
